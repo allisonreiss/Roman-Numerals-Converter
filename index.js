1a462c822147adb329bf83f1fbf04519b0dd4808
Allison Reiss <allisonreiss1@gmail.com>
2018-04-25 20:42:16 -0700

    passed tests for basic conversions to one letter

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function toRomanNumerals(number) {
-    return "I"
+    var digit = [1, 5, 10, 50, 100, 500, 1000]
+    var roman = ["I", "V", "X", "L", "C", "D", "M"]
+
+    for (var i = 0; i < digit.length; i++) {
+    	if (digit[i] == number) {
+    		var letter = roman[i];
+    	}
+    }
+
+    return letter;
 }
 
 module.exports = {
--- index.spec.js
+++ index.spec.js
@@ -5,4 +5,22 @@ describe('Test converting normal numbers to Roman Numerals', () => {
     it('Should return I when converting 1', () => {
         equal(toRomanNumerals(1), "I")
     })
+    it('Should return V when converting 5', () => {
+        equal(toRomanNumerals(5), "V")
+    })
+    it('Should return X when converting 10', () => {
+        equal(toRomanNumerals(10), "X")
+    })
+    it('Should return L when converting 50', () => {
+        equal(toRomanNumerals(50), "L")
+    })
+    it('Should return C when converting 100', () => {
+        equal(toRomanNumerals(100), "C")
+    })
+    it('Should return D when converting 500', () => {
+        equal(toRomanNumerals(500), "D")
+    })
+    it('Should return M when converting 1000', () => {
+        equal(toRomanNumerals(1000), "M")
+    })
 })
